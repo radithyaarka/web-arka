3ed2a5c84afb2087ed7a4bc54f7208f0e3f65eda
Arka Radithya <arkaradithya47@gmail.com>
2025-01-03 21:05:02 +0800

    minor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterView } from 'vue-router'; // Untuk menampilkan rute dinamis
-import Navbar from './components/Header.vue'; // Header atau Navbar
 import '@fortawesome/fontawesome-free/css/all.css';
 </script>
 
